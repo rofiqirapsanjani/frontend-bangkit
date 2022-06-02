6d3b401a67d0c3905356c70701b6b3a242ebf996
Ismail Firman <79613677+elfirman@users.noreply.github.com>
2022-06-03 01:47:33 +0700

    Update ProfileFragment.kt

--- app/src/main/java/com/example/projectcapstone/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/projectcapstone/ui/profile/ProfileFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.projectcapstone.databinding.FragmentProfileBinding
@@ -22,16 +21,12 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileViewModel =
+        val hrofileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
