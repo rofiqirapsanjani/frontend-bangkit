99c151e49fcd0e4e2c6e4b14f3f1fea7e7043d22
Ismail Firman <79613677+elfirman@users.noreply.github.com>
2022-06-04 22:20:38 +0700

    Create OrderHistoryActivity.kt

---
+++ app/src/main/java/com/example/projectcapstone/ui/orderhistory/OrderHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projectcapstone.ui.orderhistory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectcapstone.R
+
+class OrderHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_history)
+    }
+}
