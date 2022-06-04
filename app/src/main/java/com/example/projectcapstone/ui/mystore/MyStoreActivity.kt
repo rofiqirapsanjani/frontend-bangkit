6df0f5f932aa735a3775b3c34433dc67d26b2eee
Ismail Firman <79613677+elfirman@users.noreply.github.com>
2022-06-04 22:19:58 +0700

    Create MyStoreActivity.kt

---
+++ app/src/main/java/com/example/projectcapstone/ui/mystore/MyStoreActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projectcapstone.ui.mystore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectcapstone.R
+
+class MyStoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_store)
+    }
+}
