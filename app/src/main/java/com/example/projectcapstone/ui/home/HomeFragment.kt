efa6aa96b80a2e5838040d21147b0910a50b7ff1
Ismail Firman <79613677+elfirman@users.noreply.github.com>
2022-06-11 22:41:07 +0700

    Update HomeFragment.kt

--- app/src/main/java/com/example/projectcapstone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/projectcapstone/ui/home/HomeFragment.kt
@@ -1,38 +1,71 @@
 package com.example.projectcapstone.ui.home
 
 import android.os.Bundle
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectcapstone.R
+import com.example.projectcapstone.adapter.ProductsAdapter
+import com.example.projectcapstone.authentication.register.RegisterActivity
 import com.example.projectcapstone.databinding.FragmentHomeBinding
+import com.example.projectcapstone.network.response.Product
+import com.example.projectcapstone.network.response.ProductResponse
+import com.example.projectcapstone.ui.ProductDetail
+
 
 class HomeFragment : Fragment() {
+    private lateinit var rvproducts: RecyclerView
+    private lateinit var products : List<Product>
 
-    private var _binding: FragmentHomeBinding? = null
-
+    private lateinit var  _binding: FragmentHomeBinding
+    private lateinit var homeViewModel: HomeViewModel
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+        val view = inflater.inflate(R.layout.fragment_home ,container, false)
+        rvproducts = view.findViewById(R.id.rv_products)
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        getAllProducts()
+        homeViewModel.products.observe(viewLifecycleOwner){
+            products = it!!
+            showProducts(products)
+        }
+
+    }
+
+    fun getAllProducts() {
+        homeViewModel.fetchProducts()
+    }
+
+    fun showProducts(products: List<Product>){
+        rvproducts.layoutManager = LinearLayoutManager(activity)
+        val adapter = ProductsAdapter(products)
+        rvproducts.adapter = adapter
+        adapter.setOnItemClickCallback(object : ProductsAdapter.OnItemClickCallback{
+            override fun onItemClicked(product: Product) {
+                val intent = Intent(activity, ProductDetail::class.java)
+                intent.putExtra("product", product)
+                System.out.println("getclicked")
+                startActivity(intent)
+            }
+        })
+
     }
 }
