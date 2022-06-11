12469c6a074ad3e6aafb2f0cff7128b08f403124
Ismail Firman <79613677+elfirman@users.noreply.github.com>
2022-06-11 22:51:04 +0700

    Update OrderHistoryActivity.kt

--- app/src/main/java/com/example/projectcapstone/ui/orderhistory/OrderHistoryActivity.kt
+++ app/src/main/java/com/example/projectcapstone/ui/orderhistory/OrderHistoryActivity.kt
@@ -1,47 +1,12 @@
-package com.example.projectcapstone.ui.order
+package com.example.projectcapstone.ui.orderhistory
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.example.projectcapstone.databinding.ActivityOrderBinding
-import com.example.projectcapstone.network.response.Product
+import android.os.Bundle
+import com.example.projectcapstone.R
 
-
-class OrderActivity  : AppCompatActivity() {
-//    val Context.dataStore: DataStore<Preferences> by preferencesDataStore("settings")
-//    val pref = UserPreference.getInstance(dataStore)
-    private lateinit var binding: ActivityOrderBinding
-    private lateinit var orderViewModel: OrderViewModel
-    private lateinit var stripePayment : StripePayment
-
+class OrderHistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityOrderBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        orderViewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
-        stripePayment = StripePayment()
-//        lifecycleScope.launch{
-//            accessToken=pref.getPreferences()[UserPreference.]
-//        }
-        val product : Product? = intent.getParcelableExtra("infoproduct")
-        val productId =product!!.id
-        val sellerId = product!!.sellerId
-        val productPrice =product!!.price
-        val productImg =product!!.img
-        val productTitle = product!!.title
-        System.out.println("product id "+productId)
-        binding.prodid.text = product!!.id
-//        binding.prodid.text = productPrice
-//        binding.prodid.text = productImg
-//        binding.prodid.text = productTitle
-
-        val address = binding.Address.text.toString()
-        binding.pay.setOnClickListener{
-            orderViewModel.addOrder("dfdadf",sellerId, productId, productPrice, "address")
-//            stripePayment.paymentStripe(product!!.price)
-//            startActivity(Intent(this@OrderActivity, MainActivity::class.java))
-        }
+        setContentView(R.layout.activity_order_history)
     }
-
-
 }
